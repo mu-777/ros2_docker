d5478cc0fe8c3af5b9298d2ad8156f3b5b9d7732
mu-777 <water.tree.rainbow@gmail.com>
2022-02-12 22:46:40 +0900

    remove docker compose

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-export UID=$(id -u $USER)
-export GID=$(id -g $USER)
-docker-compose run foxy_desktop
+
+docker run --rm -itd \
+    -v /etc/group:/etc/group:ro \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/shadow:/etc/shadow:ro \
+    -u $(id -u $USER):$(id -g $USER) \
+    mu777/ros2:foxy_desktop bash
